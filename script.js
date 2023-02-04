9c2480a908f7c05ba85c49367ff1a725249e571d
Felipe <felipesansoliv01@gmail.com>
2023-02-04 19:33:38 -0300

    Commit dos arquivos

---
+++ script.js
@@ -0,0 +1,25 @@
+var slider = document.getElementById("servoSlider");
+    var servoP = document.getElementById("servoPos");
+    servoP.innerHTML = slider.value;
+    slider.oninput = function() {
+      slider.value = this.value;
+      servoP.innerHTML = this.value;
+    }
+    $.ajaxSetup({timeout:1000});
+    function servo(pos) {
+      $.get("/?value=" + pos + "&");
+      {Connection: close};
+    }
+    
+var slider = document.getElementById("servoDelay");
+var servoP = document.getElementById("delay");
+servoP.innerHTML = slider.value;
+slider.oninput = function() {
+      slider.value = this.value;
+      servoP.innerHTML = this.value;
+    }
+    $.ajaxSetup({timeout:1000});
+    function servo(pos) {
+      $.get("/?value=" + pos + "&");
+      {Connection: close};
+    }
