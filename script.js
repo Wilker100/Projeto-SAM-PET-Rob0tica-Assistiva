ce6fc254750496f52677ecece770c6cae163ad58
Felipe <felipesansoliv01@gmail.com>
2023-02-12 12:04:55 -0300

    Mudança de aparência

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ var slider = document.getElementById("servoSlider");
       slider.value = this.value;
       servoP.innerHTML = this.value;
     }
+
+    var sliderD = document.getElementById("delaySlider");
+    var servoD = document.getElementById("servoDelay");
+    servoD.innerHTML = sliderD.value;
+    sliderD.oninput = function() {
+      sliderD.value = this.value;
+      servoD.innerHTML = this.value;
+    }
+
+
+    
+    
     $.ajaxSetup({timeout:1000});
     function servo(pos) {
       $.get("/?value=" + pos + "&");
       {Connection: close};
     }
     
-var slider = document.getElementById("servoDelay");
-var servoP = document.getElementById("delay");
-servoP.innerHTML = slider.value;
-slider.oninput = function() {
-      slider.value = this.value;
-      servoP.innerHTML = this.value;
-    }
-    $.ajaxSetup({timeout:1000});
-    function servo(pos) {
-      $.get("/?value=" + pos + "&");
-      {Connection: close};
-    }
+    
